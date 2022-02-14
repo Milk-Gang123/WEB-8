38ce8a3d12d01da8a165fbccd31675e56b6c86cd
Администратор <alexushveridze@mail.ru>
2022-02-14 23:03:35 +0300

    profs list

--- flask-wtf/index.py
+++ flask-wtf/index.py
@@ -19,6 +19,17 @@ def training(prof):
     return render_template('training.html', **params)
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                   'инженер по терраформированию', 'климатолог',
+                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                   'штурман', 'пилот дронов']
+    params = {'list': list, 'profs': professions}
+    return render_template('list_prof.html', **params)
+
+
 
 if __name__ == "__main__":
     app.run(port=8080, host="127.0.0.1")
